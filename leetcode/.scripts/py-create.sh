1313b9c574ed034f14d18750f5fdc0eff0a92b91
Owen Knott <oknott14@outlook.com>
2025-06-04 17:07:29 -0400

    python creation script

---
+++ leetcode/.scripts/py-create.sh
@@ -0,0 +1,13 @@
+
+
+TEST_TEMPLATE='''class Solution:\n\tdef prob_function():\n\t\tpass\n\nsoln = Solution()\n\ndef test_case_1():\n\tassert true'''
+$1=$1 -replace 
+DIRECTORY_PATH="./leetcode/$1"
+TEST_FILE_PATH="$DIRECTORY_PATH/test_$1.py"
+
+bash ./leetcode/.scripts/create-problem.sh $DIRECTORY_PATH $TEST_FILE_PATH
+
+if [ -e "$TEST_FILE_PATH" ] && [ ! -s "$TEST_FILE_PATH" ]; then
+  echo "Inserting Template File"
+  cat ./leetcode/.scripts/test_template.py > $TEST_FILE_PATH
+fi
