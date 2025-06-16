b88b8eb5325db4b5e1c04d44985d224c7d98ad23
Owen Knott <oknott14@outlook.com>
2025-06-16 17:05:57 -0400

    binary tree level order traversal

--- leetcode/binary-tree-level-order-traversal/test_binary-tree-level-order-traversal.py
+++ leetcode/binary-tree-level-order-traversal/test_binary-tree-level-order-traversal.py
@@ -1,8 +1,53 @@
+from typing import Optional, List
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
-  def fn():
-    pass
+  def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+    def traverse_orders(head: Optional[TreeNode], lst: List[List[int]] = [], level: int = 0) -> None:
+      if head is None:
+        return
+      
+      if level == len(lst):
+        lst.append([])
+
+      lst[level].append(head.val)
+      level += 1
+      traverse_orders(head.left, lst, level)
+      traverse_orders(head.right, lst, level)
+      
+      return
+    
+    lst = []
+    traverse_orders(root, lst)
+    return lst
+
+
+  def build_tree(self, tree_arr: List[int | None], index: int = 0) -> Optional[TreeNode]:
+    if len(tree_arr) <= index or tree_arr[index] is None:
+      return None
+    else:
+      return TreeNode(tree_arr[index], self.build_tree(tree_arr, 2 * index + 1), self.build_tree(tree_arr, 2 * index + 2))
 
 soln = Solution()
 
 def test_case_1():
-  assert True
+  input = soln.build_tree([3,9,20,None,None,15,7])
+  output = [[3], [9, 20], [15,7]]
+
+  assert soln.levelOrder(input) == output
+
+def test_case_2():
+  input = soln.build_tree([3, 6, 10, 2, 4, 5, None, 1, 5, 7, None, 1, 2])
+  output = [[3], [6,10], [2, 4, 5], [1, 5, 7, 1, 2]]
+
+  assert soln.levelOrder(input) == output
+
+def test_case_empty():
+  input = soln.build_tree([])
+  output = []
+
+  assert soln.levelOrder(input) == output
