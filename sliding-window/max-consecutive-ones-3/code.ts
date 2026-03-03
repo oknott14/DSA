62e949d73bdd32d393e9b7612d13f3c48a4cc2dd
oknott14 <oknott14@outlook.com>
2026-03-03 09:43:18 -0500

    Max consecutive ones

--- sliding-window/max-consecutive-ones-3/code.ts
+++ sliding-window/max-consecutive-ones-3/code.ts
@@ -1,1 +1,24 @@
-function longestOnes(nums: number[], k: number): number {}
+export function longestOnes(nums: number[], k: number): number {
+	let left = 0;
+	let right = 0;
+	let longest = 0;
+	let flipped = 0;
+
+	while (right < nums.length) {
+		if (nums[right] == 0) {
+			flipped++;
+		}
+
+		if (flipped <= k) {
+			longest = Math.max(longest, right - left + 1);
+		} else {
+			if (nums[left] == 0) {
+				flipped--;
+			}
+			left++;
+		}
+		right++;
+	}
+
+	return longest;
+}
--- sliding-window/max-consecutive-ones-3/max-consecutive-ones-3.test.ts
+++ sliding-window/max-consecutive-ones-3/max-consecutive-ones-3.test.ts
@@ -1,5 +1,35 @@
-describe('{testName}', () => {
-	test('Case 1', () => {
-		expect(true).toBeTruthy();
+import { longestOnes } from './code';
+
+describe('Max Consecutive Ones 3', () => {
+	test('Empty Arr', () => {
+		expect(longestOnes([], 1)).toEqual(0);
+	});
+
+	test('K is Zero', () => {
+		expect(longestOnes([1, 1, 0, 1], 0)).toEqual(2);
+	});
+
+	test('All zeros', () => {
+		expect(longestOnes([0, 0, 0], 2)).toEqual(2);
+	});
+
+	test('Flips in the middle', () => {
+		expect(longestOnes([1, 1, 0, 1, 1], 1)).toEqual(5);
+	});
+
+	test('Flips preceding', () => {
+		expect(longestOnes([0, 1, 1], 1)).toEqual(3);
+	});
+
+	test('Flips following', () => {
+		expect(longestOnes([1, 1, 0], 1)).toEqual(3);
+	});
+
+	test('Flips many', () => {
+		expect(longestOnes([0, 1, 1, 0, 1, 1, 0], 3)).toEqual(7);
+	});
+
+	test('Selects longest ones', () => {
+		expect(longestOnes([1, 0, 1, 0, 1, 1, 1], 1)).toEqual(5);
 	});
 });
